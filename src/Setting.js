1375ca8d3627b4558bc623a73f317167cac99398
Jhagas Winaya <jhagashana@mailbox.org>
2022-01-28 21:33:36 +0700

    mobile pwa optimization

--- src/Setting.js
+++ src/Setting.js
@@ -1,6 +1,7 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
 import { IoSettingsOutline } from "react-icons/io5";
+import { FiMinus, FiPlus } from "react-icons/fi";
 
 export default function ModalSetting(props) {
   const [settings, setSettings] = props.setting;
@@ -86,77 +87,209 @@ export default function ModalSetting(props) {
                   <div className="flex flex-col gap-[0.2rem] rounded-xl overflow-hidden shadow-lg">
                     <div className="flex flex-row justify-between text-base bg-gray5 py-2 px-4 items-center">
                       <label>Lap length</label>
-                      <input
-                        value={changes.lapLength}
-                        onChange={(event) =>
-                          setChanges({
-                            ...changes,
-                            lapLength: event.target.value,
-                          })
-                        }
-                        className="outline-none bg-gray3 h-8 rounded-lg px-3"
-                        type="number"
-                        name="lapLength"
-                        id="lap"
-                        min="1"
-                        max="99"
-                      />
+                      <div className="flex flex-row gap-[0.1rem]">
+                        <input
+                          value={changes.lapLength}
+                          onChange={(event) =>
+                            setChanges({
+                              ...changes,
+                              lapLength: event.target.value,
+                            })
+                          }
+                          className="outline-none bg-gray3 h-8 rounded-l-lg text-center w-12"
+                          type="number"
+                          name="lapLength"
+                          inputMode="none"
+                          id="lap"
+                          min="1"
+                          max="99"
+                        />
+                        <div
+                          onClick={() => {
+                            if (JSON.parse(changes.lapLength) > 1) {
+                              setChanges({
+                                ...changes,
+                                lapLength:
+                                  JSON.parse(changes.lapLength) - 1,
+                              });
+                            }
+                          }}
+                          className="w-8 bg-gray3 flex flex-row justify-center items-center cursor-pointer"
+                        >
+                          <FiMinus size="14px" />
+                        </div>
+                        <div
+                          onClick={() => {
+                            if (
+                              JSON.parse(changes.lapLength) < 99
+                            ) {
+                              setChanges({
+                                ...changes,
+                                lapLength:
+                                  JSON.parse(changes.lapLength) + 1,
+                              });
+                            }
+                          }}
+                          className="w-8 bg-gray3 flex flex-row justify-center items-center cursor-pointer rounded-r-lg"
+                        >
+                          <FiPlus size="14px" />
+                        </div>
+                      </div>
                     </div>
                     <div className="flex flex-row justify-between text-base bg-gray5 py-2 px-4 items-center">
                       <label>Short break length</label>
-                      <input
-                        value={changes.shortBreakLength}
-                        onChange={(event) =>
-                          setChanges({
-                            ...changes,
-                            shortBreakLength: event.target.value,
-                          })
-                        }
-                        className="outline-none bg-gray3 h-8 rounded-lg px-3"
-                        type="number"
-                        name="lapLength"
-                        id="lap"
-                        min="1"
-                        max="99"
-                      />
+                      <div className="flex flex-row gap-[0.1rem]">
+                        <input
+                          value={changes.shortBreakLength}
+                          onChange={(event) =>
+                            setChanges({
+                              ...changes,
+                              shortBreakLength: event.target.value,
+                            })
+                          }
+                          className="outline-none bg-gray3 h-8 rounded-l-lg text-center w-12"
+                          type="number"
+                          inputMode="none"
+                          name="lapLength"
+                          id="lap"
+                          min="1"
+                          max="99"
+                        />
+                        <div
+                          onClick={() => {
+                            if (JSON.parse(changes.shortBreakLength) > 1) {
+                              setChanges({
+                                ...changes,
+                                shortBreakLength:
+                                  JSON.parse(changes.shortBreakLength) - 1,
+                              });
+                            }
+                          }}
+                          className="w-8 bg-gray3 flex flex-row justify-center items-center cursor-pointer"
+                        >
+                          <FiMinus size="14px" />
+                        </div>
+                        <div
+                          onClick={() => {
+                            if (
+                              JSON.parse(changes.shortBreakLength) < 99
+                            ) {
+                              setChanges({
+                                ...changes,
+                                shortBreakLength:
+                                  JSON.parse(changes.shortBreakLength) + 1,
+                              });
+                            }
+                          }}
+                          className="w-8 bg-gray3 flex flex-row justify-center items-center cursor-pointer rounded-r-lg"
+                        >
+                          <FiPlus size="14px" />
+                        </div>
+                      </div>
                     </div>
                     <div className="flex flex-row justify-between text-base bg-gray5 py-2 px-4 items-center">
                       <label>Long break length</label>
-                      <input
-                        value={changes.longBreakLength}
-                        onChange={(event) =>
-                          setChanges({
-                            ...changes,
-                            longBreakLength: event.target.value,
-                          })
-                        }
-                        className="outline-none bg-gray3 h-8 rounded-lg px-3"
-                        type="number"
-                        name="lapLength"
-                        id="lap"
-                        min="1"
-                        max="99"
-                      />
+                      <div className="flex flex-row gap-[0.1rem]">
+                        <input
+                          value={changes.longBreakLength}
+                          onChange={(event) =>
+                            setChanges({
+                              ...changes,
+                              longBreakLength: event.target.value,
+                            })
+                          }
+                          className="outline-none bg-gray3 h-8 rounded-l-lg text-center w-12"
+                          type="number"
+                          name="lapLength"
+                          inputMode="none"
+                          id="lap"
+                          min="1"
+                          max="99"
+                        />
+                        <div
+                          onClick={() => {
+                            if (JSON.parse(changes.longBreakLength) > 1) {
+                              setChanges({
+                                ...changes,
+                                longBreakLength:
+                                  JSON.parse(changes.longBreakLength) - 1,
+                              });
+                            }
+                          }}
+                          className="w-8 bg-gray3 flex flex-row justify-center items-center cursor-pointer"
+                        >
+                          <FiMinus size="14px" />
+                        </div>
+                        <div
+                          onClick={() => {
+                            if (
+                              JSON.parse(changes.longBreakLength) < 99
+                            ) {
+                              setChanges({
+                                ...changes,
+                                longBreakLength:
+                                  JSON.parse(changes.longBreakLength) + 1,
+                              });
+                            }
+                          }}
+                          className="w-8 bg-gray3 flex flex-row justify-center items-center cursor-pointer rounded-r-lg"
+                        >
+                          <FiPlus size="14px" />
+                        </div>
+                      </div>
                     </div>
                   </div>
                   <div className="flex flex-col gap-1 rounded-xl overflow-hidden mt-6 shadow-lg">
                     <div className="flex flex-row justify-between text-base bg-gray5 py-2 px-4 items-center">
                       <label>Session until long break</label>
-                      <input
-                        value={changes.sessionUntilLongBreak}
-                        onChange={(event) =>
-                          setChanges({
-                            ...changes,
-                            sessionUntilLongBreak: event.target.value,
-                          })
-                        }
-                        className="outline-none bg-gray3 h-8 rounded-lg px-3"
-                        type="number"
-                        name="lapLength"
-                        id="lap"
-                        min="2"
-                        max="10"
-                      />
+                      <div className="flex flex-row gap-[0.1rem]">
+                        <input
+                          value={changes.sessionUntilLongBreak}
+                          onChange={(event) =>
+                            setChanges({
+                              ...changes,
+                              sessionUntilLongBreak: event.target.value,
+                            })
+                          }
+                          className="outline-none bg-gray3 h-8 rounded-l-lg text-center w-12"
+                          type="number"
+                          name="lapLength"
+                          id="lap"
+                          min="2"
+                          inputMode="none"
+                          max="16"
+                        />
+                        <div
+                          onClick={() => {
+                            if (JSON.parse(changes.sessionUntilLongBreak) > 1) {
+                              setChanges({
+                                ...changes,
+                                sessionUntilLongBreak:
+                                  JSON.parse(changes.sessionUntilLongBreak) - 1,
+                              });
+                            }
+                          }}
+                          className="w-8 bg-gray3 flex flex-row justify-center items-center cursor-pointer"
+                        >
+                          <FiMinus size="14px" />
+                        </div>
+                        <div
+                          onClick={() => {
+                            if (
+                              JSON.parse(changes.sessionUntilLongBreak) < 16
+                            ) {
+                              setChanges({
+                                ...changes,
+                                sessionUntilLongBreak:
+                                  JSON.parse(changes.sessionUntilLongBreak) + 1,
+                              });
+                            }
+                          }}
+                          className="w-8 bg-gray3 flex flex-row justify-center items-center cursor-pointer rounded-r-lg"
+                        >
+                          <FiPlus size="14px" />
+                        </div>
+                      </div>
                     </div>
                   </div>
                   <div className="mt-4 flex flex-row gap-2">
